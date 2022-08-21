975af6ddb8e562e63c8e93d3b23aa30c6cdca6fe
boularbahsmail <qiranorane55@gmail.com>
2022-08-22 00:03:03 +0100

    Files has been moved out from Day_0 folder (deleted)

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAME="Rick Sanshez"
+SPORT="Foot"
+
+echo "My name is ${NAME} and i play ${SPORT}ball"
+
+# The curly braces are important when it comes to concatenating data
+# In this case we concatenate the variable $SPORT with the string "ball"
+
+STUDENT_01="Morty Smith"
