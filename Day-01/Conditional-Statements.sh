b423249c0113cde378b5073ba5232eec296e90b9
boularbahsmail <qiranorane55@gmail.com>
2022-08-22 15:16:58 +0100

    Conditional Statements

---
+++ Day-01/Conditional-Statements.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Username : " NAME
+
+if [ "$NAME" = "Ismailium" ];
+then
+    echo "Welcome back Ismailium"
+else
+    echo "Invalid username, Please register an account!!"
+fi # End the statement
