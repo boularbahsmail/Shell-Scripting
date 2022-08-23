d4e5ad3baa546b0389a6aef7b0766f6ced392d88
boularbahsmail <qiranorane55@gmail.com>
2022-08-23 13:30:46 +0100

    Foor Loops

---
+++ Day-02/For-Loops.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for NAMES in $(cat Names.txt); do # Reading Names from Names.txt file
+    echo "$NAMES"
+done
+
+# CAT COMMAND : It increases the speed and performance of the script when it comes to reading files content.
